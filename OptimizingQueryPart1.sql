0699050397ebf5f9206b435f893e6b5a18d82ee9
Amin Siddique <44993406+Amin-Siddique@users.noreply.github.com>
2022-02-05 19:57:07 +0530

    Update OptimizingQueryPart1.sql

--- OptimizingQueryPart1.sql
+++ OptimizingQueryPart1.sql
@@ -1,3 +1,8 @@
 ##Select only columns that are required:  (use case of except)
 
 select * except(last_name,first_order_date) from `bigquery-demo-329104.analytics.customer` 
+
+
+## Using partitioning and clustering to prune out data which is not required.
+
+## Try to avoid group by whenever possible, instead use nested queries and utiliste array_length to get the aggregation if required.
